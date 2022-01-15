bfacc56b54a473cc04e3813e277693b5906a220e
MatanPazi <74129593+MatanPazi@users.noreply.github.com>
2022-01-15 12:51:52 +0200

    Update VFD_Testing_PCB.ino

--- VFD_Testing_PCB.ino
+++ VFD_Testing_PCB.ino
@@ -25,6 +25,9 @@
    https://lastminuteengineers.com/tm1637-arduino-tutorial/
    //Atmega328 pin numbers:
    http://www.learningaboutelectronics.com/Articles/Atmega328-pinout.php
+   Dead-time:
+   Datasheet recommends 1 [us] for each input signal. However, when looking at the datasheet, the turn-on time is 290[ns] and turn off time is 515[ns], so I need a minimum of
+   515-290 = 225[ns] between PWM signals. Each count takes 125[ns], so I'll take 125*4 = 500[ns] dead time.
    //
    //To-do *****************************************************************************
    Save last phase configuration to EEPROM. ********************************************
