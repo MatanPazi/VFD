b54369eb17d31b976308326ff3e2833a2803cb30
MatanPazi <74129593+MatanPazi@users.noreply.github.com>
2022-10-14 12:27:59 +0300

    Perform division outside of interrupt
    Reduce interrupt time.

--- VFD.ino
+++ VFD.ino
@@ -113,6 +113,7 @@ uint8_t Sine_Index = 0;                   //3 sine wave indices are used to allo
 uint8_t Sine_Index_120 = Sine_Len / 3;
 uint8_t Sine_Index_240 = (Sine_Len * 2) / 3;       //Sine_Len must be lower than 128, otherwise, change eq. 
 uint8_t OVF_Counter = 0;                  //Increments every Timer0 overflow
+uint8_t OVF_Counter_Compare = 0;          //Compare OVF_Counter to this value (Base freq/ desired freq)
 
 TM1637Display Display1(CLK1, DIO1);
 TM1637Display Display2(CLK2, DIO2);
@@ -146,6 +147,7 @@ void loop()
    Desired_Freq = ((uint8_t)(analogRead(POT_INPUT) >> 3));   //A value of 1023 (5V) -> 128[Hz]. Divide result by 8 to get value in Hz. Gives resolution of 1[Hz]
    if (Desired_Freq < Min_Freq) Desired_Freq = Min_Freq;
    else if (Desired_Freq > Max_Freq) Desired_Freq = Max_Freq;
+   OVF_Counter_Compare = (uint8_t)(Base_Freq / Desired_Freq)
    Amp = ((float)(Desired_Freq) * V_f) / VBus;                 //Calculating the sine wave amplitude based on the desired frequency and the V/f value.
    if (Amp < Min_Amp) Amp = Min_Amp;      
    else if (Amp > Max_Amp) Amp = Max_Amp;
@@ -390,7 +392,7 @@ ISR (TIMER0_OVF_vect)
    else if (PWM_Running == PWM_RUNNING)
    {
       OVF_Counter++;   
-      if (OVF_Counter >= ((uint8_t)(Base_Freq / Desired_Freq)))
+      if (OVF_Counter >= OVF_Counter_Compare)
       {
          if (Sine_Index == Sine_Len) Sine_Index = 0;
          if (Sine_Index_120 == Sine_Len) Sine_Index_120 = 0;
