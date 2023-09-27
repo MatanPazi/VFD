e42bf6f0e41986f447c0530bae1f7a0639cf5485
Matan <74129593+MatanPazi@users.noreply.github.com>
2023-09-27 20:34:07 +0300

    Adding animated plots.

--- Id_Iq.py
+++ Id_Iq.py
@@ -4,15 +4,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 import Transformations as trs
+import matplotlib.animation as animation
 
-omega = 2*np.pi*300
-t = np.linspace(0,0.01,1000)
-t2 = t[1:-1]
-R = 0.005
-L = 0.00001
+# parameter_to_change = 'none'
+# parameter_to_change = 'speed'
+parameter_to_change = 'idiq'
+
+OMEGA = 2*np.pi*50
+N = 1000
+T = 1
+t = np.linspace(0,T,N)
+omega = OMEGA*np.ones(N)
+chg_index = N//2
+
+if (parameter_to_change == 'speed'):
+    omega[chg_index : -1] *= 2
+
+R = 0.5
+L = 0.001
 
 # BEMF
-Ke = -0.0006
+Ke = -0.6
 KeA = Ke * np.sin(omega*t)              # Sinusoidal BEMF
 KeB = Ke * np.sin(omega*t - np.deg2rad(120))              # Sinusoidal BEMF
 KeC = Ke * np.sin(omega*t - np.deg2rad(240))              # Sinusoidal BEMF
@@ -22,7 +34,10 @@ BEMFC = KeC * omega                      # BEMF
 
 # Desired current vector
 I = 100
-dqAng = np.deg2rad(90)      # dqAng = 0[deg] -> Only Id. dqAng = 90[deg]  ->  Only Iq
+dqAng = np.deg2rad(90)*np.ones(N)        # dqAng = 0[deg] -> Only Id. dqAng = 90[deg]  ->  Only Iq
+if (parameter_to_change == 'idiq'):
+    dqAng[chg_index : -1] *= 0.3
+
 Iq = I * np.sin(dqAng)
 Id = -I * np.cos(dqAng)
 
@@ -54,13 +69,61 @@ Vb = DeltaVb + BEMFB
 Vc = DeltaVc + BEMFC
 
 
-# It can be seen very nicely, that with only Iq, the voltage (V) amplitude required is higher than with an added Id element.
-plt.figure()
-plt.xlabel('Time [Sec]')
-plt.ylabel('[A] OR [V*100]')
-plt.plot(t[1:-1],100*DeltaVa[1:-1], label='V-BEMF*100')
-plt.plot(t[1:-1],100*BEMFA[1:-1], label='BEMFA*100')
-plt.plot(t,Ia, label='Ia')
-plt.plot(t[1:-1],100*(Va[1:-1]), label='V')
-plt.legend()
+# # It can be seen very nicely, that with only Iq, the voltage (V) amplitude required is higher than with an added Id element.
+# plt.figure()
+# plt.xlabel('Time [Sec]')
+# plt.ylabel('[A] OR [V*100]')
+# plt.plot(t[1:-1],100*DeltaVa[1:-1], label='V-BEMF*100')
+# plt.plot(t[1:-1],100*BEMFA[1:-1], label='BEMFA*100')
+# plt.plot(t,Ia, label='Ia')
+# plt.plot(t[1:-1],100*(Va[1:-1]), label='V')
+# plt.legend()
+# plt.show()
+
+
+# Animating the graphs
+fig, (ax, ax_chg) = plt.subplots(2,1)
+# Find min max values of represented data
+max_y = int(max(max(Ia), max(BEMFA), max(DeltaVa), max(Va))) + 10
+min_y = int(min(min(Ia), min(BEMFA), min(DeltaVa), min(Va))) - 10
+
+line1 = ax.plot(t[1], Ia[1], label='Ia[A]')[0]
+line2 = ax.plot(t[1], BEMFA[1], label='BEMFA[V]')[0]
+line3 = ax.plot(t[1], DeltaVa[1], label='DrivingV - BEMF [V]')[0]
+line4 = ax.plot(t[1], Va[1], label='DrivingV [V]')[0]
+ax.set(xlim=[0, T], ylim=[min_y, max_y], xlabel='Time [s]', ylabel='Parameters')
+ax.legend()
+
+if (parameter_to_change == 'idiq'):
+    line5 = ax_chg.plot(t[1], Iq[1], label='Iq [A]')[0]
+    line6 = ax_chg.plot(t[1], Id[1], label='Id [A]')[0]
+    ax_chg.set(xlim=[0, T], ylim=[min(-10, min(Id - 10)), max(max(Iq), max(Id)) + 10], xlabel='Time [s]', ylabel='Current [A]')
+    ax_chg.legend()
+else:
+    line6 = ax_chg.plot(t[1], omega[1], label='Omega[Rad/sec]')[0]
+    ax_chg.set(xlim=[0, T], ylim=[-10, max(omega)+10], xlabel='Time [s]', ylabel='Omega [rad/sec]')
+    ax_chg.legend()
+
+
+def update(frame):
+    # update the line plot:
+    line1.set_xdata(t[:frame])
+    line1.set_ydata(Ia[:frame])
+    line2.set_xdata(t[:frame])
+    line2.set_ydata(BEMFA[:frame])
+    line3.set_xdata(t[:frame])
+    line3.set_ydata(DeltaVa[:frame])
+    line4.set_xdata(t[:frame])
+    line4.set_ydata(Va[:frame])
+    line5.set_xdata(t[:frame])
+    if (parameter_to_change == 'idiq'):
+        line5.set_ydata(Iq[:frame])
+        line6.set_xdata(t[:frame])
+        line6.set_ydata(Id[:frame])
+        return (line1, line2, line3, line4, line5, line6)
+    else:
+        line5.set_ydata(omega[:frame])
+    return (line1, line2, line3, line4, line5)
+
+ani = animation.FuncAnimation(fig=fig, func=update, interval=1, frames=N)
 plt.show()
